e48305bbdd5b6e0a2de19ffd505ffd1d9cb760f8
wkh <1531685233@qq.com>
2024-12-26 12:03:50 +0800

    fix: duplicate CallExpression

--- src/plugin/insert-plugin.js
+++ src/plugin/insert-plugin.js
@@ -20,29 +20,6 @@ const insertPlugin = ({ types, template }, options, dirname) => {
         if (path.node.isNew) {
           return;
         }
-        const calleeName = path.get("callee").toString();
-        if (targetCalleeName.includes(calleeName)) {
-          const { line, column } = path.node.loc.start;
-          const newNode = template.expression(
-            `console.log("${
-              state.filename || "unkown filename"
-            }: (${line}, ${column})")`
-          )();
-          newNode.isNew = true;
-
-          if (path.findParent((path) => path.isJSXElement())) {
-            path.replaceWith(types.arrayExpression([newNode, path.node]));
-            path.skip();
-          } else {
-            path.insertBefore(newNode);
-          }
-        }
-      },
-
-      CallExpression(path, state) {
-        if (path.node.isNew) {
-          return;
-        }
 
         const calleeName = path.get("callee").toString();
 
